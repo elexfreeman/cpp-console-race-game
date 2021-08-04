0432d0aff0de633ed4db9896faba436c7b3998e4
alex <AAnPolozov@sberbank.ru>
2021-08-04 13:38:47 +0300

    click thread

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <thread>
+#include <future>
 
 #include <ctime>
 #include <cmath>
@@ -11,6 +13,39 @@
 
 #include "ScreenG.h"
 #include "Mesh.h"
+
+void fOnClick(int nKey)
+{
+    switch (nKey)
+    {
+    case ERR:
+        // printw("Please, press any key...\n"); //Если нажатия не было, напоминаем пользователю, что надо нажать клавишу
+        break;
+    case KEY_F(2): //Выходим из программы, если была нажата F2
+        break;
+    case KEY_UP:
+        printw("U");
+        break;
+    case KEY_DOWN:
+        printw("D");
+        break;
+    case KEY_LEFT:
+        printw("L");
+        break;
+    case KEY_RIGHT:
+        printw("R");
+        break;
+    default: //Если всё нормально, выводим код нажатой клавиши
+        printw("Code of pressed key is %d\n", nKey);
+        break;
+    }
+}
+
+void foo()
+{
+    // do stuff...
+    printw("Code of pressed key is %d\n", 1);
+}
 
 int main()
 {
@@ -22,7 +57,7 @@ int main()
     initscr();
     keypad(stdscr, true); //Включаем режим чтения функциональных клавиш
     noecho();             //Выключаем отображение вводимых символов, нужно для getch()
-    halfdelay(1);       //Устанавливаем ограничение по времени ожидания getch() в 10 сек
+    halfdelay(1);         //Устанавливаем ограничение по времени ожидания getch() в 10 сек
 
     ScreenG *scr = new ScreenG;
 
@@ -53,18 +88,8 @@ int main()
         scr->fPrint();
 
         nCh = getch();
-
-        switch (nCh)
-        {
-        case ERR:
-            printw("Please, press any key...\n"); //Если нажатия не было, напоминаем пользователю, что надо нажать клавишу
-            break;
-        case KEY_F(2): //Выходим из программы, если была нажата F2
-            break;
-        default: //Если всё нормально, выводим код нажатой клавиши
-            printw("Code of pressed key is %d\n", nCh);
-            break;
-        }
+        std::thread vThreadClick(fOnClick, nCh);
+        vThreadClick.join();
 
         mvaddch(0, 0, 'W');
         mvaddch(0, 1, 'R');
